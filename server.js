47771660d0f0e8768c643b27345ad8eccedbb5d5
boot2boot2022-website <boot2boot2022@gmail.com>
2026-04-22 16:52:38 +0530

    pause logic

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use(cors({
     "http://localhost:5173",
   ],
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
 app.use(helmet({
