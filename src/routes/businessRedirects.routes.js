b156e67b59fb96979c9e713f806e4ef49fca3f63
boot2boot2022-website <boot2boot2022@gmail.com>
2026-05-15 12:18:45 +0530

    businessRedirects.routes changes

--- src/routes/businessRedirects.routes.js
+++ src/routes/businessRedirects.routes.js
@@ -1,7 +1,9 @@
+import crypto from "crypto";
 import express from "express";
 import Project from "../models/Project.model.js";
 
 const router = express.Router();
+global.sessions = global.sessions || {};
 
 router.get("/start", async (req, res) => {
   const { tk } = req.query;
@@ -21,6 +23,22 @@ router.get("/start", async (req, res) => {
     return res.send("Survey not Live");
   }
 
+   const sid = crypto.randomBytes(16).toString("hex");
+
+ 
+  global.sessions[sid] = {
+    projectId: project._id.toString(),
+    used: false,
+    ip: req.ip,
+    ua: req.headers["user-agent"],
+    createdAt: Date.now(),
+  };
+
+  res.cookie("sid", sid, {
+    httpOnly: true,
+    maxAge: 1000 * 60 * 60, 
+    sameSite: "lax",
+  });
 
   if (project.completes >= project.targetCompletes) {
     return res.redirect(`/api/redirect/qf?tk=${project.redirects.quotaFull.token}`);
@@ -34,12 +52,33 @@ router.get("/start", async (req, res) => {
 
 router.get("/c", async (req, res) => {
   const { tk } = req.query;
+  const sid = req.cookies.sid;
+  if(!sid){
+    return res.send("No Session");
+  }
+
+  const session = global.sessions[sid];
+   if (!session || session.used) {
+    return res.send("Invalid or reused session");
+  }
 
   const project = await Project.findOne({
     "redirects.complete.token": tk,
   });
 
   if (!project) return res.send("Invalid");
+
+  if (session.projectId !== project._id.toString()) {
+    return res.send("Session mismatch");
+  }
+
+  // same IP + browser check
+  if (
+    session.ip !== req.ip ||
+    session.ua !== req.headers["user-agent"]
+  ) {
+    return res.send("Fingerprint mismatch");
+  }
 
   // if (project.completes >= project.targetCompletes) {
   //   return res.redirect(`/api/redirect/qf?tk=${project.redirects.quotaFull.token}`);
@@ -59,6 +98,8 @@ router.get("/c", async (req, res) => {
   return res.redirect("https://inputify.io/quota-full");
   }
 
+  session.used = true;
+  
   await Project.updateOne(
     { _id: project._id },
     { $inc: { completes: 1, totalResponses: 1, }, }
