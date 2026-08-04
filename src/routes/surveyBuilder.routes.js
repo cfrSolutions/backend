b473667e331f12021997d4c24a417479350efbe5
mitrivVentures <mitriv03@gmail.com>
2026-08-04 12:10:19 +0530

    surveyRoutes and surveyBuilder

--- src/routes/surveyBuilder.routes.js
+++ src/routes/surveyBuilder.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import auth from "../middleware/auth.js";
-import { createSurvey } from "../controllers/surveyController.js";
+import { createSurvey } from "../controllers/surveyBuilder.controller.js";
 
 const router = express.Router();
 
