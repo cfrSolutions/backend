36f9d03d3415d833611f7fe8e2e292ec48410084
Harsha Shigwan <harshashigwan595@gmail.com>
2026-08-24 17:09:54 +0530

    auth.middleware.js

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -39,7 +39,11 @@ export const authMiddleware = async (req, res, next) => {
   // }
 
   // const token = req.cookies.token;
-  const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+  // const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+  const token =
+  req.headers.authorization?.startsWith("Bearer ")
+    ? req.headers.authorization.split(" ")[1]
+    : req.cookies.token;
 
   if (!token) {
     return res.status(401).json({ message: "Unauthorized" });
