c901750be763653acfb9fa8b6f8a23ee5a0ee908
Harsha Shigwan <harshashigwan595@gmail.com>
2026-09-21 12:30:55 +0530

    server.js

--- server.js
+++ server.js
@@ -218,6 +218,6 @@ io.on("connection", (socket) => {
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => {
+server.listen(PORT, '127.0.0.1', () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
